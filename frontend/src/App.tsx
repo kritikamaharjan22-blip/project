bdac7d24bf6c2cac107d90ce0fde33c1a8cba90c
unknown <kritikamaharjan22@gmail.com>
2026-04-13 08:55:14 +0545

    message here

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+      <div className='helo'>
+        <p>hello</p>
+        </div>
+      </section>
+
+    </>
+  )
+}
+
+export default App
